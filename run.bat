ccf42a05dd0589ca5aef38dc64862c5834addf42
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2022-04-19 15:40:05 +0200

    shell and batch scripts

--- run.bat
+++ run.bat
@@ -1,12 +1,7 @@
-# Take run arguments
-echo "Do you want to process a video or an image [v/i]?"
-read type_to_process
-echo "Enter a relative path to the input:"
-read in_path
-echo "Enter a relative path to the output:"
-read out_path
-echo "Do you want to enable debug mode [y/n]?"
-read debug_mode
+set /p type_to_process="Do you want to process a video or an image [v/i]?"
+set /p in_path="Enter a relative path to the input:"
+set /p out_path="Enter a relative path to the output:"
+set /p debug_mode="Do you want to enable debug mode [y/n]?"
 
-# run the python file
-python3 main.py $type_to_process $in_path $out_path $debug_mode
+
+python main.py %type_to_process% %in_path% %out_path% %debug_mode%
