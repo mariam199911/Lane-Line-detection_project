f7dfd7320756a08d46e16f6dece2a5b8b51d10ca
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2022-05-20 01:50:04 +0200

    patch file

---
+++ stage2.bat
@@ -0,0 +1,6 @@
+set type_to_process=%1
+set in_path=%2
+set out_path=%3
+set debug_mode=%4
+
+python stage2.py %type_to_process% %in_path% %out_path% %debug_mode%
---
+++ stage2.py
@@ -0,0 +1,255 @@
+import numpy as np
+import cv2
+from skimage.feature import hog
+import pickle
+from scipy.ndimage import label
+from moviepy.editor import VideoFileClip
+import os
+import sys
+
+def convert_color(img, conv='RGB2YCrCb'):
+    """
+    Convert the image from one color space to the other
+    """
+    if conv == 'RGB2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    if conv == 'BGR2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    if conv == 'RGB2LUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    if conv == 'RGB2HLS':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    if conv == 'RGB2HSV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    if conv == 'Gray':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if conv == 'RGB2YUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
+                        vis=False, feature_vec=True):
+    """
+    Return the hog features of the given input image
+    Call with two outputs if vis==True"""
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                                  visualize=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
+                       visualize=vis, feature_vector=feature_vec)
+        return features
+
+def load(data_file):
+    global svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, detection_info
+    
+    dist_pickle = pickle.load(open(f"{data_file}", "rb" ))
+    
+    svc = dist_pickle["svc"]
+    X_scaler = dist_pickle["scaler"]
+    orient = dist_pickle["orient"]
+    pix_per_cell = dist_pickle["pix_per_cell"]
+    cell_per_block = dist_pickle["cell_per_block"]
+    spatial_size = dist_pickle["spatial_size"]
+    hist_bins = dist_pickle["hist_bins"]
+
+
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, vis_bboxes = False):
+    
+    draw_img = np.copy(img)
+    xstart = int(img.shape[1]/5)
+    xstop = img.shape[1]
+    img_tosearch = img[ystart:ystop, xstart:xstop,:]
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YUV')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (int(imshape[1]/scale), int(imshape[0]/scale)))
+
+    ch1 = ctrans_tosearch[:,:,0]
+    ch2 = ctrans_tosearch[:,:,1]
+    ch3 = ctrans_tosearch[:,:,2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1 
+    
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+    cells_per_step = 2
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    rectangles = []
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb*cells_per_step
+            xpos = xb*cells_per_step
+            
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3)).reshape(1, -1)
+
+            xleft = xpos*pix_per_cell
+            ytop = ypos*pix_per_cell
+
+            test_features = X_scaler.transform(hog_features)   
+            test_prediction = svc.predict(test_features)
+            
+            if test_prediction == 1 or vis_bboxes == True:
+                xbox_left = int(xleft*scale)
+                ytop_draw = int(ytop*scale)
+                win_draw = int(window*scale)
+                rectangles.append(((xbox_left+xstart, ytop_draw+ystart),(xbox_left+win_draw+xstart,ytop_draw+win_draw+ystart)))
+                              
+    return rectangles
+
+
+def get_rectangles(image, scales = [1, 1.5, 2, 2.5, 3], 
+                   ystarts = [400, 400, 450, 450, 460], 
+                   ystops = [528, 550, 620, 650, 700]):
+    global svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins
+
+    out_rectangles = []
+    for scale, ystart, ystop in zip(scales, ystarts, ystops):
+        rectangles = find_cars(image, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins)
+        if len(rectangles) > 0:
+            out_rectangles.append(rectangles)
+    out_rectangles = [item for sublist in out_rectangles for item in sublist] 
+    return out_rectangles
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    img_copy = np.copy(img)
+    result_rectangles = []
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        area = (bbox[1][1] - bbox[0][1]) * (bbox[1][0] - bbox[0][0])
+        if area > 40 * 40:
+            result_rectangles.append(bbox)
+            # Draw the box on the image
+            cv2.rectangle(img_copy, bbox[0], bbox[1], (0,255,0), 6)
+    # Return the image
+    return result_rectangles, img_copy
+
+def find_vehicles(image):
+    rectangles = get_rectangles(image)
+    if not rectangles or len(rectangles) == 0:
+        return image
+
+    heatmap_image = np.zeros_like(image[:, :, 0])
+    heatmap_image = add_heat(heatmap_image, rectangles)
+    heatmap_image = apply_threshold(heatmap_image, 2)
+    labels = label(heatmap_image)
+    rectangles, result = draw_labeled_bboxes(image, labels)
+
+    return result
+
+def process_video(in_path, out_path, debug):
+    # detection_info = DetectionInfo()
+
+    project_video = VideoFileClip(in_path)
+    white_clip = project_video.fl_image(find_vehicles)
+    white_clip.write_videofile(out_path, audio=False, fps=20)
+
+def process_image(in_path, out_path, debug):
+    # detection_info = DetectionInfo()
+
+    # process image
+    image = cv2.imread(in_path)
+
+    result = find_vehicles(image)
+
+    # Save image
+    cv2.imwrite(f'{out_path}', result)
+    
+
+def main():
+    args = sys.argv
+    try:
+        in_type = str(args[1])
+        in_path = str(args[2])
+        out_path = str(args[3])
+        debug_mode = int(args[4])
+        debug = False
+
+        # check if the input directory exists or not
+        (in_dir, in_file) = os.path.split(in_path)
+        in_dir = os.path.relpath(in_dir)
+        if not os.path.exists(in_dir):
+            print(f"{in_dir} is not found.")
+            return
+
+        # check if the output directory exists or create new one
+        (out_dir, out_file) = os.path.split(out_path)
+        out_dir = os.path.relpath(out_dir)
+        if len(out_dir) > 0 and not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+            print(f"{out_dir} is created.")
+            
+        if debug_mode == 1:
+            debug = True
+        
+        # load the model
+        data_file = 'svc_pickle.sav'
+        load(data_file)
+        
+        if in_type == 'v' or in_type == 'video' or in_type == 'V' or in_type == 'Video':
+            print('Processing video......')
+            process_video(in_path, out_path, debug)
+        elif in_type == 'i' or in_type == 'image' or in_type == 'I' or in_type == 'Image':
+            print('Processing image......')
+            process_image(in_path, out_path, debug)
+        else:
+            print('Incorrect input type')
+            return
+        print(f'Processing done, please check {out_path}')
+
+    except IndexError:
+        print('Incorrect args')
+        return
+
+svc = None
+X_scaler = None
+orient = None
+pix_per_cell = None
+cell_per_block = None
+spatial_size = None
+hist_bins = None
+detection_info = None
+
+if __name__ == '__main__':
+    main()
